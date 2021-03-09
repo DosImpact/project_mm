025f21e05e6531a2e352dba11c3de34e6f047caf
DosImpact <ypd03008@gmail.com>
2021-03-09 12:14:50 +0900

    Problem02Controller

--- mm_back/src/problems/problems.module.ts
+++ mm_back/src/problems/problems.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Problem01 } from './entities/problem01.entity';
 import { Problem02 } from './entities/problem02.entity';
-import { ProblemsController } from './problems.controller';
-import { Problem01Resolver } from './problems.resolver';
+import { Problem01Controller } from './problems.controller';
+import { Problem01Resolver, Problem02Resolver } from './problems.resolver';
 import { ProblemsService } from './problems.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Problem01, Problem02])],
-  providers: [Problem01Resolver, ProblemsService],
-  controllers: [ProblemsController],
+  providers: [Problem01Resolver, Problem02Resolver, ProblemsService],
+  controllers: [Problem01Controller],
 })
 export class ProblemsModule {}
