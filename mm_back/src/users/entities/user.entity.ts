0fe6662e8a150cf63a06e23306f96d404c72872b
dosimpact <ypd03008@gmail.com>
2021-03-21 12:18:49 +0900

    CreateUserInput

--- mm_back/src/users/entities/user.entity.ts
+++ mm_back/src/users/entities/user.entity.ts
@@ -1,8 +1,10 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
-import { IsString } from 'class-validator';
+import { IsBoolean, IsString } from 'class-validator';
 import { CoreEntity } from 'src/common/entities/core.entity';
-import { Column, Entity, OneToOne } from 'typeorm';
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToOne } from 'typeorm';
 import { Profile } from './profile.entity';
+import { compare, hash, genSalt } from 'bcrypt';
+import { InternalServerErrorException } from '@nestjs/common';
 
 @InputType({ isAbstract: true })
 @ObjectType()
@@ -10,14 +12,41 @@ import { Profile } from './profile.entity';
 export class User extends CoreEntity {
   @IsString()
   @Field(() => String)
-  @Column({ default: '' })
+  @Column({ default: '', nullable: true })
   username: string;
 
   @IsString()
   @Field(() => String)
-  @Column({ unique: true, nullable: true })
+  @Column({ unique: true })
   email: string;
+
+  @IsString()
+  @Field(() => String)
+  @Column()
+  password: string;
+
+  @IsBoolean()
+  @Field(() => Boolean)
+  @Column({ default: false })
+  verified: boolean;
 
   @OneToOne(() => Profile, (profile) => profile.user)
   profile: Profile;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword() {
+    try {
+      if (this.password) {
+        const salt = await genSalt();
+        this.password = await hash(this.password, salt);
+      }
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async checkPassword(plainPassword: string) {
+    return compare(plainPassword, this.password);
+  }
 }
