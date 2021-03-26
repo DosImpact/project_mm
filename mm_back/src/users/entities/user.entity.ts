b29499f2d404f1b12a3f65fab75f2fdd9abca0f6
dosimpact <ypd03008@gmail.com>
2021-03-26 12:45:56 +0900

    pytaskmodeling & redis

--- mm_back/src/users/entities/user.entity.ts
+++ mm_back/src/users/entities/user.entity.ts
@@ -1,10 +1,18 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { IsBoolean, IsString } from 'class-validator';
 import { CoreEntity } from 'src/common/entities/core.entity';
-import { BeforeInsert, BeforeUpdate, Column, Entity, OneToOne } from 'typeorm';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  OneToMany,
+  OneToOne,
+} from 'typeorm';
 import { Profile } from './profile.entity';
 import { compare, hash, genSalt } from 'bcrypt';
 import { InternalServerErrorException } from '@nestjs/common';
+import { PyTask } from '@/pyshell/entities/py-task.dto';
 
 @InputType({ isAbstract: true })
 @ObjectType()
@@ -30,8 +38,14 @@ export class User extends CoreEntity {
   @Column({ default: false })
   verified: boolean;
 
+  //relation part
+  // 한명의 유저는 하나의 프로필을 갖는다.
   @OneToOne(() => Profile, (profile) => profile.user)
   profile: Profile;
+
+  // 한명의 유저는 요청한 여러 작업결과를 갖는다.
+  @OneToMany(() => PyTask, (pyTask) => pyTask.user)
+  pyTasks: PyTask[];
 
   @BeforeInsert()
   @BeforeUpdate()
